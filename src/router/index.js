b431d3e386c105f8949b47566d580e3ebfea2f91
Jhordy Mondragon Saucedo <jhordymondragon4@gmail.com>
2024-09-22 04:22:21 -0500

    Set scroll position to top on route change

--- src/router/index.js
+++ src/router/index.js
@@ -85,6 +85,11 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    return new Promise(resolve => {
+      resolve({ top: 0 });
+    });
+  }
 });
 
 let authInitialized = false;
